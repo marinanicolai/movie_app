981205625daf973308f250059f8e49c5709f3a32
marina <nicolaimarina@gmail.com>
2021-01-17 18:58:58 -0500

    update

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 
-const SEARCH_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=02ce28bcb0320d007a30c63a29d122ba&QUERY="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=02ce28bcb0320d007a30c63a29d122ba&query="'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 //Get initial movies
 getMovies(API_URL)
@@ -11,5 +15,53 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
 }
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        <div class="movie">
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="green">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+                saepe temporibus esse illum assumenda in nemo similique modi 
+                illo distinctio nulla magni laboriosam debitis voluptatum 
+                exercitationem neque voluptas, veritatis placeat aliquid 
+                quidem! Ullam, dolorem amet?
+            </div>
+        </div>
+        `
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value 
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+
+})
